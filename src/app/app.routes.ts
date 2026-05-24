09440b34aebf7c4daac3a2f82d2c103ee81954d6
GabrielForestiero <gabyforestiero@gmail.com>
2026-05-24 19:02:55 -0300

    vista configuracion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { GameComponent } from './game/game.component';
 import { RegistroComponent } from './auth/registro/registro.component';
 import { LoginComponent } from './auth/login/login.component';
+import { ConfiguracionComponent } from './configuracion/configuracion.component';
 
 export const routes: Routes = [
   { path: '', component: GameComponent },
   { path: 'registro', component: RegistroComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'configuracion', component: ConfiguracionComponent },
   { path: '**', redirectTo: '' },
 ];
