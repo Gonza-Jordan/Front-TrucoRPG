84be42e8475da393bb9bd3d6109deab362c67adf
MeliAbi <mabillera@alumno.unlam.edu.ar>
2026-05-25 23:19:30 -0300

    Nueva rama desde develop

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,12 @@ import { LoginComponent } from './auth/login/login.component';
 import { Home } from './pages/home/home';
 import { LandingPage } from './pages/landing-page/landing-page';
 import { ConfiguracionComponent } from './pages/configuracion/configuracion';
+import { MenuMultijugador } from './pages/menu-multijugador/menu-multijugador';
 
 export const routes: Routes = [
   { path: '', component: LandingPage, data: { header: 'landing' } },
   { path: 'juego/:modo', component: GameComponent },
+  { path: 'menu-multijugador', component: MenuMultijugador, data: { header: 'menu-multijugador' } },
   { path: 'registro', component: RegistroComponent, data: { header: 'registro' } },
   { path: 'login', component: LoginComponent, data: { header: 'login' } },
   { path: 'home', component: Home, data: { header: 'home' } },
