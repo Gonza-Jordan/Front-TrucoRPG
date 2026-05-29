f6652cdba6190f60dda010201207e1e33bd20199
MeliAbi <mabillera@alumno.unlam.edu.ar>
2026-05-29 00:26:51 -0300

    Adaptacion y mejorado de menu multijugador, mj tradicional y mj crear sala

--- src/app/pages/menu-multijugador-tradicional-sala/menu-multijugador-tradicional-sala.ts
+++ src/app/pages/menu-multijugador-tradicional-sala/menu-multijugador-tradicional-sala.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { Header } from '../../components/header/header';
 import { Footer } from '../../components/footer/footer';
 import { RouterLink } from '@angular/router';
+import { ConnectionStatusComponent } from '../../components/connection-status/connection-status';
 
 @Component({
   selector: 'app-menu-multijugador-tradicional-sala',
-  imports: [Header,Footer,RouterLink],
+  imports: [Header,Footer,RouterLink,ConnectionStatusComponent],
   templateUrl: './menu-multijugador-tradicional-sala.html',
   styleUrl: './menu-multijugador-tradicional-sala.css',
 })
