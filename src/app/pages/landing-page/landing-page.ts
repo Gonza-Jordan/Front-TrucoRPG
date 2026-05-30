590cce280c255be615d761bf9d106ab11360a77d
ppilarodriguez <pilar.rodriguez2430@gmail.com>
2026-05-30 19:39:44 -0300

    Saco botón flotante de volver + acomodo imagen del dorso de la tarjeta

--- src/app/pages/landing-page/landing-page.ts
+++ src/app/pages/landing-page/landing-page.ts
@@ -1,11 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Header } from "../../components/header/header";
-import { Footer } from "../../components/footer/footer";
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [Header, Footer, RouterLink],
+  imports: [ RouterLink],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.css',
 })
