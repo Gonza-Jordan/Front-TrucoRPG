d623e04f3140126feb88900eca6eb1f5d088c1a2
ppilarodriguez <pilar.rodriguez2430@gmail.com>
2026-06-06 15:20:29 -0300

    Subo la primer versión del mapa

--- src/game/escenas/HistoriaBootScene.js
+++ src/game/escenas/HistoriaBootScene.js
@@ -11,7 +11,22 @@ export default class HistoriaBootScene extends Phaser.Scene {
         this.load.spritesheet('nene-arco','./assets/sprites/nene-arco.png',{frameWidth:64,frameHeight:64});
         this.load.spritesheet('nene-hacha','./assets/sprites/nene-hacha.png',{frameWidth:64,frameHeight:64});
 
-        this.load.image('pasto','./assets/objetos/pasto.png');
+        this.load.tilemapTiledJSON('mapa','./assets/mapa-principal/mapa-principal.json');
+
+        this.load.image('Arbol 1','./assets/mapa-principal/Arbol1.png');
+        this.load.image('Arbol 2','./assets/mapa-principal/Arbol2.png');
+        this.load.image('Arbol 3','./assets/mapa-principal/Arbol3.png');
+        this.load.image('Fuego','./assets/mapa-principal/Fogata.png');
+        this.load.image('Gallinas','./assets/mapa-principal/Gallinitas.png');
+        this.load.image('Mate','./assets/mapa-principal/mate.png');
+        this.load.image('Paredes','./assets/mapa-principal/Paredes.png');
+        this.load.image('Partes','./assets/mapa-principal/Partes.png');
+        this.load.image('Pava','./assets/mapa-principal/pavita.png');
+        this.load.image('Piedras','./assets/mapa-principal/Piedras.png');
+        this.load.image('Piso 2','./assets/mapa-principal/Camino.png');
+        this.load.image('Techos','./assets/mapa-principal/Techos.png');
+        this.load.image('Vegetacion','./assets/mapa-principal/Vegetacion.png');
+
         
         this.load.plugin('rexvirtualjoystickplugin', 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexvirtualjoystickplugin.min.js', true);
     }
--- src/game/escenas/MapaPrincipalScene.js
+++ src/game/escenas/MapaPrincipalScene.js
@@ -8,17 +8,41 @@ export default class MapaPrincipalScene extends BaseScene {
 
   init(data) {
     this.playerKey = data.playerSprite || 'player';
-    this.startX = data.x || 100;
-    this.startY = data.y || 100;
+    this.startX = data.x || 85;
+    this.startY = data.y || 470;
   }
 
   create() {
     this.botonPantallaCompleta();
     this.crearControlesMobile();
 
-    this.fondo = this.add
-      .tileSprite(0, 0, this.scale.width, this.scale.height, 'pasto')
-      .setOrigin(0, 0);
+    // A PARTIR DE ACÁ LA CREACION DEL MAPA CON TILEEEED 
+    const map = this.make.tilemap({ key: 'mapa' });
+
+    const pisoTileset = map.addTilesetImage('Piso 2', 'Piso 2');
+    const arbol1Tileset = map.addTilesetImage('Arbol 1', 'Arbol 1');
+    const arbol2Tileset = map.addTilesetImage('Arbol 2', 'Arbol 2');
+    const arbol3Tileset = map.addTilesetImage('Arbol 3', 'Arbol 3');
+    const fuegoTileset = map.addTilesetImage('Fuego', 'Fuego');
+    const gallinasTileset = map.addTilesetImage('Gallinas', 'Gallinas');
+    const mateTileset = map.addTilesetImage('Mate', 'Mate');
+    const paredesTileset = map.addTilesetImage('Paredes', 'Paredes');
+    const partesTileset = map.addTilesetImage('Partes', 'Partes');
+    const pavaTileset = map.addTilesetImage('Pava', 'Pava');
+    const piedrasTileset = map.addTilesetImage('Piedras', 'Piedras');
+    const techosTileset = map.addTilesetImage('Techos', 'Techos');
+    const vegetacionTileset = map.addTilesetImage('Vegetacion', 'Vegetacion');
+
+    map.createLayer('Base', pisoTileset);
+    map.createLayer('Pasto',[piedrasTileset,vegetacionTileset]);
+    map.createLayer('Camino',pisoTileset);
+    map.createLayer('Arboles',[vegetacionTileset,arbol1Tileset]);
+    map.createLayer('Arboles 2',[arbol2Tileset]);
+    map.createLayer('Arboles 3',[arbol3Tileset,partesTileset]);
+    map.createLayer('Casas',[paredesTileset,techosTileset,fuegoTileset]);
+    map.createLayer('Gallinitas', [gallinasTileset]);
+    map.createLayer('Objetos-Casa',[partesTileset,mateTileset,pavaTileset,fuegoTileset]);
+    map.createLayer('Gallinitas 2', [gallinasTileset]);
 
     this.JugadorPrincipal = new JugadorPrincipal(
       this,
