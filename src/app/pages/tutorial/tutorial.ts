09b44e7a427343352446026a542bd1355bef526b
angela quelca <anahi.laurani@gmail.com>
2026-06-08 21:53:24 -0300

    saque codigo que ya no funciona con las nuevas implementaciones

--- src/app/pages/tutorial/tutorial.ts
+++ src/app/pages/tutorial/tutorial.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { PageWrapper } from '../../components/page-wrapper/page-wrapper';
-import { Reglas } from '../../interfaces/reglas';
-import { TutorialService } from "../../services/tutorial.service";
-import { Cartas } from '../../interfaces/cartas';
 
 @Component({
   selector: 'app-tutorial',
@@ -11,54 +8,6 @@ import { Cartas } from '../../interfaces/cartas';
   templateUrl: './tutorial.html',
   styleUrl: './tutorial.css',
 })
-export class Tutorial implements OnInit {
-
-constructor(private tutorialService: TutorialService) {}
-
-  pasoActual = 0;
-  
-  reglas : Reglas[] = [];
-  cartas : Cartas[] = [];
-
-  ngOnInit(): void {
-    this.cargarReglas();
-  }
-
-  cargarReglas(): void {
-    this.tutorialService.obtenerReglas().subscribe({
-      next: (reglas) =>{
-        this.reglas = reglas;
-
-      },
-      error: (error) => {
-        console.error('Error al cargar las reglas del truco:', error);
-      }
-    });
-  }
-
-  cargarCartas(): void {
-    this.tutorialService.obtenerCartas().subscribe({
-      next: (cartas) =>{
-        this.cartas = cartas;},
-      error: (error) => {
-        console.error('Error al cargar las cartas del truco:', error);
-      } 
-    });
-  }
-
-
-  siguiente():void{
-    if(this.pasoActual === 0){
-      this.pasoActual = 1;
-      this.cargarCartas();
-    }
-  }
-
-  anterior():void{
-    if(this.pasoActual === 1){
-      this.pasoActual = 0;
-      this.cargarReglas();
-    } 
-  }
+export class Tutorial  {
 
 }
