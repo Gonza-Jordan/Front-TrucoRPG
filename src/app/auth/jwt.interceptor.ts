562aa148489d46bc1558174f64ea4f5878edb2f6
Gabriela <leverattomariag@gmail.com>
2026-06-09 22:48:12 -0300

    se eliminan comentarios

--- src/app/auth/jwt.interceptor.ts
+++ src/app/auth/jwt.interceptor.ts
@@ -6,15 +6,15 @@ export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
   const token = authService.obtenerToken();
 
-  // Si tenemos un token guardado, clonamos la petición original y le inyectamos la cabecera
+
   if (token) {
     req = req.clone({
       setHeaders: {
-        Authorization: `Bearer ${token}` // Es CLAVE que diga 'Bearer ' con el espacio antes del token
+        Authorization: `Bearer ${token}`
       }
     });
   }
 
-  // Dejamós que la petición siga su camino hacia el backend
+
   return next(req);
 };
