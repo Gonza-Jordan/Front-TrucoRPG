fc712349fe27d62ed588dd094f0b07fc0a2ee99d
ppilarodriguez <pilar.rodriguez2430@gmail.com>
2026-06-15 18:15:17 -0300

    Cambios en la selección de avataress + responsive

--- src/game/data/personaje.js
+++ src/game/data/personaje.js
@@ -1,8 +1,8 @@
 export const PERSONAJES = [
-    { id: 0, nombrePersonaje: 'Primero', spriteKey: 'personaje' },
-    { id: 1, nombrePersonaje: 'Segundo', spriteKey: 'personaje' },
-    { id: 2, nombrePersonaje: 'Tercero', spriteKey: 'personaje' },
-    { id: 3, nombrePersonaje: 'Cuarto', spriteKey: 'personaje' }
+    { id: 0, nombrePersonaje: 'Primero', spriteKey: 'personaje', img:'/Imagenes/avatares/personaje1.png' },
+    { id: 1, nombrePersonaje: 'Segundo', spriteKey: 'personaje', img:'/Imagenes/avatares/personaje2.png'},
+    { id: 2, nombrePersonaje: 'Tercero', spriteKey: 'personaje', img:'/Imagenes/avatares/personaje1.png'},
+    { id: 3, nombrePersonaje: 'Cuarto', spriteKey: 'personaje', img:'/Imagenes/avatares/personaje2.png'}
 ];
 
 export function personajePorId(id) {
