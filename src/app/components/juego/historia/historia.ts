3e61556a4c491c8ea13952e3f6fc539488272e3c
agusmd1 <agustin.m.dominguez@gmail.com>
2026-06-18 14:10:09 -0300

    add prologo en la historia

--- src/app/components/juego/historia/historia.ts
+++ src/app/components/juego/historia/historia.ts
@@ -4,39 +4,46 @@ import { SeleccionPersonajeHistoria } from '../../../pages/seleccion-personaje-h
 import { HistoriaService } from '../../../services/historia/historia-service';
 import { CasaManager } from '../../casaManager/casa-manager/casa-manager';
 import { PulperiaManager } from '../../pulperiaManager/pulperia-manager/pulperia-manager';
+import { Prologo } from '../../../pages/prologo/prologo';
 
 @Component({
   selector: 'app-historia',
   standalone: true,
-  imports: [CommonModule, SeleccionPersonajeHistoria, CasaManager,PulperiaManager],
+  imports: [CommonModule, SeleccionPersonajeHistoria, CasaManager, PulperiaManager, Prologo],
   templateUrl: './historia.html',
   styleUrl: './historia.css',
 })
 export class Historia implements OnDestroy {
-  vistaActual: 'seleccion-heroe' | 'en-juego' = 'seleccion-heroe';
+  //vistaActual: 'seleccion-heroe' | 'en-juego' = 'seleccion-heroe';
+  vistaActual: 'seleccion-heroe' | 'prologo' | 'en-juego' = 'seleccion-heroe';
 
   constructor(
     private historiaService: HistoriaService,
     @Inject(DOCUMENT) private document: Document,
-  ) {}
+  ) { }
 
   alConfirmarHeroe(evento: { heroeId: number; habilidad: string }): void {
     this.historiaService.setHeroeSeleccionado(evento.heroeId);
     this.historiaService.setHabilidadSeleccionada(evento.habilidad);
 
-    this.vistaActual = 'en-juego';
+    this.vistaActual = 'prologo';
 
     this.document.body.classList.add('modo-phaser-mobile');
 
-    setTimeout(() => {
-      this.historiaService.iniciarJuego('historia-container');
-
-      window.dispatchEvent(new Event('resize'));
-    }, 50);
   }
 
   ngOnDestroy(): void {
     this.historiaService.destruirJuego();
     this.document.body.classList.remove('modo-phaser-mobile');
+  }
+
+
+  alTerminarPrologo(): void {
+    this.vistaActual = 'en-juego';
+    this.document.body.classList.add('modo-phaser-mobile');
+    setTimeout(() => {
+      this.historiaService.iniciarJuego('historia-container');
+      window.dispatchEvent(new Event('resize'));
+    }, 50);
   }
 }
