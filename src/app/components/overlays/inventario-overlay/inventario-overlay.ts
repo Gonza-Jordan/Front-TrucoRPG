6e577f2bf7da2ac6d543ad32215e3cddeff79a0b
ppilarodriguez <pilar.rodriguez2430@gmail.com>
2026-06-20 20:30:33 -0300

    Agrego vista inventario (sin lógica y con datos hardcodeados)

--- src/app/components/overlays/inventario-overlay/inventario-overlay.ts
+++ src/app/components/overlays/inventario-overlay/inventario-overlay.ts
@@ -1,12 +1,56 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-inventario-overlay',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './inventario-overlay.html',
   styleUrl: './inventario-overlay.css',
 })
 export class InventarioOverlay {
-  @Input() datos: any;
+  inventarioCompleto = [
+    {
+      id: 1,
+      nombre: 'Boleadoras de Manipulador',
+      categoria: 'HABILIDADES',
+      cantidad: 1,
+      img: '/assets/objetos/objeto.png',
+    },
+    {
+      id: 5,
+      nombre: 'Poncho rosa',
+      categoria: 'ARMARIO',
+      cantidad: 1,
+      img: '/assets/objetos/GotaRosa.png',
+    },
+    {
+      id: 6,
+      nombre: 'Poncho marrón',
+      categoria: 'ARMARIO',
+      cantidad: 1,
+      img: '/assets/objetos/GotaMarron.png',
+    },
+  ];
 
+  categoriaActiva: string = 'TODO';
+
+  totalSlots: number = 12;
+
+  seleccionarCategoria(categoria: string) {
+    this.categoriaActiva = categoria;
+  }
+
+  get itemsFiltrados() {
+    if (this.categoriaActiva === 'TODO') {
+      return this.inventarioCompleto;
+    }
+    return this.inventarioCompleto.filter((item) => item.categoria === this.categoriaActiva);
+  }
+
+  get slotsVacios() {
+    const cantidadItemsFiltrados = this.itemsFiltrados.length;
+    const faltantes = this.totalSlots - cantidadItemsFiltrados;
+    return faltantes > 0 ? new Array(faltantes) : [];
+  }
 }
