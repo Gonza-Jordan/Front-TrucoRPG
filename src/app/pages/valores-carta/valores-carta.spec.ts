ca3c0e82e1d371a8ceb53bbe65b64c859387f908
angela quelca <anahi.laurani@gmail.com>
2026-06-26 16:58:56 -0300

    espero que de verde

--- src/app/pages/valores-carta/valores-carta.spec.ts
+++ src/app/pages/valores-carta/valores-carta.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ValoresCarta } from './valores-carta';
+import { provideRouter } from "@angular/router";
 import { provideHttpClient } from "@angular/common/http";
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 
@@ -10,7 +11,7 @@ describe('ValoresCarta', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ValoresCarta],
-      providers: [provideHttpClient(), provideHttpClientTesting()]
+      providers: [provideRouter([]),provideHttpClient(), provideHttpClientTesting()]
     })
     .compileComponents();
 
